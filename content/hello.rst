043fcb9c4193a75206183b819ecac58b42660575
Thomas Naughton <naughtont@ornl.gov>
2024-10-02 16:22:33 -0400

    pelican (python SSG) based version
    Signed-off-by: Thomas Naughton <naughtont@ornl.gov>

---
+++ content/hello.rst
@@ -0,0 +1,16 @@
+My Hello Page
+#############
+
+:date: 2024-10-02 4:11
+:modified: 2024-10-04 4:12
+:tags: hello, www
+:category: web
+:slug: my-hello-post
+:authors: Thomas Naughton
+:summary: Brief hello page
+
+
+This is a site based on `Pelican Static Site Generator <https://getpelican.com>`_,
+which is a Python based SSG tool similar to Jekyll (written in Ruby).
+
+
---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+AUTHOR = 'Thomas Naughton'
+SITENAME = 'Thomas Naughton'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'US/Eastern'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("ornl", "https://www.csm.ornl.gov/~naughton/"),
+)
+#    ("Pelican", "https://getpelican.com/"),
+
+# Social widget
+SOCIAL = (
+    ("github", "https://github.com/naughtont3/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
